185ed24ac7d2f7ce289ddede78a6aa7f3ac355bf
mnartstuff <68624179+mnartstuff@users.noreply.github.com>
2022-02-21 03:36:57 -0600

    Update myscript.js

--- week3-seatReservation/myscript.js
+++ week3-seatReservation/myscript.js
@@ -1,4 +1,18 @@
 (function(){
 'use strict';
 
+for(let abc = 97; abc <= ; abc++){
+
+// create div.label text=abc  --append to section#left
+
+for(let num = 1; num <= 15; num++){
+// createElement(div) // id and content are abc + num
+// if or switch: append to corresponding section
+}
+ // another div.label with abc added to end 
+
+}
+// num counter for-loop: ends when num is
+//
+
 }());
