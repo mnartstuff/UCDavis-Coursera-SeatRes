8c9b29886a7e54c7a0e89650f1fbb1e8a3e35970
mnartstuff <68624179+mnartstuff@users.noreply.github.com>
2022-02-21 03:39:59 -0600

    Update myscript.js

--- week3-seatReservation/myscript.js
+++ week3-seatReservation/myscript.js
@@ -2,17 +2,20 @@
 'use strict';
 
 for(let abc = 97; abc <= ; abc++){
-
-// create div.label text=abc  --append to section#left
+// const row = String.fromCharCode(abc);
+ 
+// create div.label text=row  --append to section#left
 
 for(let num = 1; num <= 15; num++){
-// createElement(div) // id and content are abc + num
+// createElement(div) // id and content are row + num
 // if or switch: append to corresponding section
 }
  // another div.label with abc added to end 
 
 }
-// num counter for-loop: ends when num is
 //
 
 }());
+
+// String.fromCharCode(num)
+//charCodeAt()
